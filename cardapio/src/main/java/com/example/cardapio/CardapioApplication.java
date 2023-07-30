6f6b97d68cc3751062a16f642721118d39616d93
josedhonatas <dhonatas@academico.ufs.br>
2023-07-30 20:48:49 -0300

    get e post

--- cardapio/src/main/java/com/example/cardapio/CardapioApplication.java
+++ cardapio/src/main/java/com/example/cardapio/CardapioApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardapioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CardapioApplication.class, args);
 	}
